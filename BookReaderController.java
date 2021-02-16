e9dd43327397bc788337abb13b24d1f5daaa594a
johanwulf <johanwulf98@gmail.com>
2021-02-16 19:18:11 +0100

    code cleanup

--- BookReaderController.java
+++ BookReaderController.java
@@ -15,42 +15,35 @@ public class BookReaderController {
     private SortedListModel listModel;
     private JList<SortedListModel> listView;
 
-    JScrollPane scrollPane;
-    JFrame frame;
-    Container pane;
-
     public BookReaderController() {
         SwingUtilities.invokeLater(() -> createWindow("BookReader", 1000, 500));
     }
 
-
-    // Maybe this method?
     private void createWindow(String title, int width, int height) {
+        // createLists(counter);
 
-        frame = new JFrame(title);
-        pane = frame.getContentPane();
+        JFrame frame = new JFrame(title);
+        Container pane = frame.getContentPane();
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+
+        JScrollPane scrollPane = new JScrollPane(listView, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+        JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollPane.setPreferredSize(new Dimension(width, height));
+
+        pane.add(scrollPane, BorderLayout.NORTH);
         pane.add(createRadioButtons(), BorderLayout.CENTER);
         pane.add(createSearchField(frame), BorderLayout.SOUTH);
-        
 
         frame.pack();
         frame.setVisible(true);
     }
 
     private void createLists(GeneralWordCounter counter, JFrame frame) {
-        
         listModel = new SortedListModel(counter.getWordList());
         listView = new JList<SortedListModel>(listModel);
         listView.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-
-        scrollPane = new JScrollPane(listView, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-        scrollPane.setPreferredSize(new Dimension(1000, 500));
-        pane.add(scrollPane, BorderLayout.NORTH);
-
-        listView.revalidate();
+        SwingUtilities.updateComponentTreeUI(frame);
     }
 
     private JPanel createRadioButtons() {
@@ -128,301 +121,31 @@ public class BookReaderController {
 
         return searchPanel;
     }
-}
 
-public E remove(int index) {
-    if(index < 0 || index > size) {
-        throw new IndexOutOfBoundsException();
-    }
+    private void createWordCounters(String textFile, String bannedFile, JFrame frame) throws FileNotFoundException {
+        Scanner s = new Scanner(new File(textFile));
+		Scanner s2 = new Scanner(new File(bannedFile));
 
-    if(index == 0) {
-        E e = first.element;
-        first = first.next;
-        return e;
-    }
+		Set<String> banned = new HashSet<String>();
+		
+		while(s2.hasNext()) {
+			String word = s2.next().toLowerCase();		
+            banned.add(word);
+		}
+		
+		GeneralWordCounter wordCount = new GeneralWordCounter(banned);
+		
+		s.findWithinHorizon("\uFEFF", 1);
+		s.useDelimiter("(\\s|,|\\.|:|;|!|\\?|'|\\\")+");
 
-    Node<E> n = first;
+		while (s.hasNext()) {
+			String word = s.next().toLowerCase();	
+            wordCount.process(word);
+		}
 
-    for(int i = 0; i < index-1; i++) {
-        n = n.next;
-    }
-
-    E e = n.next.element;
-    n.next = n.next.next;
-    return e;
-}
-
-// Invertera vektor
-public static <E> void reverse(E[] a) {
-    reverse(0, a.length-1, a);
-}
-
-private E[] reverse(int start, int end, E[] a) {
-    if(start < end) {
-        swap(start, end, a);
-        reverse(start+1, end-1, a);
-    }
-}
-
-private E[] swap(int index1, int index2, E[] a) {
-    E temp = a[index1];
-    a[index1] = a[index2];
-    a[index2] = temp;
-    return a;
-}
-
-// Jämföra med/utan comparator
-private int compareElements(E e1, E e2) {
-    if (comparator == null) {
-        return ((Comparable<E>) e1).compareTo(e2);
-    } else {
-        return comparator.compare(e1, e2);
-    }
-}
-
-// Enkellänkad lsita
-public void clear() {
-    first = null;
-    last = null;
-    size = 0;
-}
-
-public void drop(int n) {
-    if (n >= size) {
-        clear();
-    } else {
-        for (int i = 0; i < n; i++) {
-            first = first.next;
-        }
-        size = size - n;
-    }
-}
-
-// Returnera lista med löv från binärt träd
-public List<E> leaves() {
-    List<E> list = new ArrayList();
-    leaves(root, list);
-    return list;
-}
-
-public void leaves(Node<E> n, List<E> list) {
-    if (n == null) {
-        return;
-    } else if (n.left == null && n.right == null) {
-        list.add(n.element);
-    } else {
-        leaves(n.left, list);
-        leaves(n.right, list);
-    }
-}
-
-/**
- * 1a) Falskt - eftersom att det minsta talet sätts i roten så kommer inget hamna på vänster sida av trädet vilket gör det obalanserat
- * 1b) Sant - tidskomplexiteten för denna algoritmen är kvadratisk. Det innebär att en ökning med 5 gånger av n gör att tiden blir 5 i kvadrat - alltså 25 gånger längre. 4*25 = 100.
- * 1c) Sant - bara man sätter in elementen sist i vektorn och håller koll på antalet insatta element så är de möjligt
- * 1d) Falskt - när man söker i en enkellänkad lista måste alla element gås genom vilket gör att binär sökning ej passar bra.
- * 1e) Falskt - heapsort delar inte upp problemet i mindre delar och slår ihop resultaten vilket är kännetecknande för en divide and conquer algoritm
- * 1f) Fel svar :(
- * */
-
-// 2a)
-private void add(E e) {
-    ListNode<E> temp = new ListNode<E>(e);
-    ListNode<E> current = first;
-    ListNode<E> previous = null;
-
-    if (first == null) {
-        first = temp;
-    }
-
-    while(e.compareTo(current.element) < 0) {
-        previous = current;
-        current = current.next;
-    }
-
-    previous.next = temp;
-    temp.next = current;
-}
-
-// 2b)
-private E peek() {
-    if(first == null) {
-        return null;
-    } else {
-        return first.element;
-    }
-}
-
-private E poll() {
-    if (first == null) {
-        return null;
-    } else {
-        E temp = first.element;
-        first = first.next;
-        return temp;
-    }
-}
-
-/**
- * 2c) Minheap, barnen ska vara >= föräldrern (binärt träd). Lagras i en vektor efter plats i trädet. 
- */
-
-// 3a
-public class MapSparseVector implements SparseVector {
-    int length;
-    Map<Integer, Double> map;
-
-    /**
-     * Creates a sparse vector with length elements, all with the value zero.
-     * @param length number of elements
-     */
-    public MapSparseVector(int length) {
-        this.map = new HashMap<Integer, Double>();
-        this.length = length;
-    }
-
-    /** Puts val in element with index i.
-     * @param i index
-     * @param val the new value
-     * @throws IllegalArgumentException if i < 0 or >= the length of this vector
-     */
-    public void put(int i, double val) {
-        if(i < 0 || i >= length) {
-            throw new IllegalArgumentException();
-        }
-        if(val == 0) {
-            map.remove(i);
-        } else {
-            map.put(i, val);
-        }
-    }
-
-    /**
-     * Gets the value in the element with index i.
-     * @param i index
-     * @return the value in the element with index i
-     * @throws IllegalArgumentException if i < 0 or >= the length of this vector
-     */
-    public double get(int i) {
-        if(i < 0 || i >= length) {
-            throw new IllegalArgumentException();
-        }
-
-        if(map.containsKey(i)) {
-            return map.get(i);
-        } else {
-            return 0.0;
-        }
-    }
-
-    /**
-     * Returns the length of this vector.
-     * @return the length of this vector
-     */
-    public int length() {
-        return length;
-    }
-
-    /**
-     * Computes the dot product of this vector and v.
-     * @param v the other vector
-     * @return the dot product
-     * @throws IllegalArgumentException if the two vectors have different length
-     */
-    public double dot(SparseVector v) {
-        if (v.length() != length) {
-            throw new IllegalArgumentException();
-        }
+		s.close();
+        s2.close();
         
-        double sum = 0.0;
-
-        for(int i = 0; i < length; i++) {
-            sum += this.get(i)*v.get(i);
-        }
-
-        return sum;
+        createLists(wordCount, frame);
     }
 }
-
-// 3b) hashCode används för att hitta index för det vi vill sätta in. Equals används för att kolla nycklarna
-// 3c) TreeMap
-
-public void printBiggerThan(E x) {
-    recPrint(x, root);
-}
-
-private void recPrint(E x, Node<E> n) {
-    if(n != null) {
-
-    }
-}
-
-/**
- * 1a) En generisk klass är en klass som inte har en typ. Dessa används för att metoder ska kunna användas av flera olika typer (t.ex. string och integer).
- * 1b) Ja, trädet är balanserat.
- * 1c) Rehashing innebär att man gör om hashtabellen så att den blir dubbel så stor. Detta görs för att dra ner på antalet kollisioner i tabellen. 10 -> 20 i storlek t.ex.
- * 1d) Det minsta elementet i minheapen är noden. När denna tas bort så ersätts den med det senast insatta elementet som "bubblas ner" till trädet är balanserat igen
- * 1e) Valet av pivotelement är viktigt så att man inte sorterar om i onödan. Om man t.ex. väljer första elementet som pivotelement måste man kolla genom alla platser i vektorn
- */
-
-// 2a)
-public void deleteDuplicates() {
-    ListNode<E> current = first;
-    ListNode<E> previous = null;
-
-    while(current != null) {
-        previous = current;
-        current = current.next;
-
-        if(previous.element.compareTo(current.element) == 0) {
-            previous.next = current.next;
-        }
-    }
-}
-
-// 2b) Man hade behövt jämföra varje element mot varje element, vilket innebär att vi hade fått en tidskomplexitet n^2. Värstafallet inträffar när de lika elementen är på motsatta ändar av lsitan.
-// 2c)
-public E middleVal() {
-    ListNode<E> middle = first;
-    ListNode<E> end = first;
-
-    while(end.next != null) {
-        end = end.next.next;
-        middle = middle.next;
-    }
-
-    return middle.element;
-}
-
-// 3a)
-public int getWeight() {
-    return recGetWeight(root);
-}
-
-private int recGetWeight(Node n) {
-    if(n == null) {
-        return 0;
-    } else if (n.left == null && n.right == null) {
-        return n.data;
-    } else {
-        int weight = 0;
-        weight += recGetWeight(n.left);
-        weight += recGetWeight(n.right);
-
-        return weight;
-    }
-}
-
-// 3b)
-public boolean mobileTree() {
-    if (root == null || (root.left == null && root.right == null)) {
-        return true;
-    }
-
-    int leftWeight = recGetWeight(n.left);
-    int rightWeight = recGetWeight(n.right);
-
-    return leftWeight == rightWeight;
-}
-
-// 4a)
